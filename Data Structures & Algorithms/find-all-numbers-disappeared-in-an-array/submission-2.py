b2a07ad27006331b8fc0ec9e92c2bbf37e8c3c61
Edofo <48387251+Edofo@users.noreply.github.com>
2026-04-30 16:16:38 +0200

    Add: find-all-numbers-disappeared-in-an-array - submission-2

---
+++ Data Structures & Algorithms/find-all-numbers-disappeared-in-an-array/submission-2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        for num in nums:
+            idx = abs(num) - 1
+            nums[idx] = -abs(nums[idx])
+        
+        missing = []
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                missing.append(i + 1)
+        return missing
