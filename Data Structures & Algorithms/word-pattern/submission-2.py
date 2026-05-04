7e2b6f81e648fe24817d1fbf6fbcc9d7a54947dd
Edofo <48387251+Edofo@users.noreply.github.com>
2026-05-04 19:07:57 +0200

    Add: word-pattern - submission-2

---
+++ Data Structures & Algorithms/word-pattern/submission-2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        tab = [""]
+
+        for letter in s:
+            if letter == " ":
+                tab.append("")
+                continue
+            tab[len(tab) - 1] += letter
+
+        mapping1 = {}
+        mapping2 = {}
+
+        if len(tab) != len(pattern):
+            return False
+
+        for i in range(len(tab)):
+            if pattern[i] in mapping1 and mapping1[pattern[i]] != tab[i]:
+                return False
+            if tab[i] in mapping2 and mapping2[tab[i]] != pattern[i]:
+                return False
+            mapping1[pattern[i]] = tab[i]
+            mapping2[tab[i]] = pattern[i]
+
+        return True
