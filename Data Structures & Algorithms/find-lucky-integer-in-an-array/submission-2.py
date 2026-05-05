e7c3167d75d78cb8c850c9a3df102e23a4f35478
Edofo <48387251+Edofo@users.noreply.github.com>
2026-05-05 18:22:48 +0200

    Add: find-lucky-integer-in-an-array - submission-2

---
+++ Data Structures & Algorithms/find-lucky-integer-in-an-array/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findLucky(self, arr: List[int]) -> int:
+        mapping = {}
+        for el in arr:
+            if el in mapping:
+                mapping[el] += 1
+            else:
+                mapping[el] = 1
+
+        highest = -1
+        for el in mapping:
+            if el == mapping[el] and el > highest:
+                highest = el
+
+        return highest
