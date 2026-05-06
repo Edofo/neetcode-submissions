78c6a2fd3df390dc3d859c09f39b60103977721f
Edofo <48387251+Edofo@users.noreply.github.com>
2026-05-06 20:46:27 +0200

    Add: divide-array-into-equal-pairs - submission-2

---
+++ Data Structures & Algorithms/divide-array-into-equal-pairs/submission-2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def divideArray(self, nums: List[int]) -> bool:
+        count = set()
+
+        for num in nums:
+            if num in count:
+                count.remove(num)
+            else:
+                count.add(num)
+
+        return len(count) == 0
